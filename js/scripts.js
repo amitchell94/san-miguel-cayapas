04bc89e391a693527443d6992b240d4c6a32f395
Andy <andymitchell1@live.co.uk>
2022-10-14 16:27:00 -0500

    added favicon and updated hotel page

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,15 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// let imagesToLoad = document.querySelectorAll("img[data-src]");
+// const loadImages = (image) => {
+//   image.setAttribute("src", image.getAttribute("data-src"));
+//   image.onload = () => {
+//     image.removeAttribute("data-src");
+//   };
+// };
+
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
